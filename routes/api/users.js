ce33462946a1176a16a8acc40eec985d0e64ec6f
Linda Ilonze <linda.ilonze@gmail.com>
2017-07-31 02:23:05 +0100

    updating user api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,16 @@
+
 import mongoose from 'mongoose';
 import express from 'express';
 import passport from 'passport';
 import auth from '../../auth';
 import user from '../../models/User';
+import book from '../../models/Book';
 
 const User = mongoose.model('User');
+const Book = mongoose.model('Book');
 const router = express.Router();
 
-router.post('/users',function(req,res,next){
+router.post('/',function(req,res,next){
     var user = new User();
     user.username = req.body.user.username;
     user.email = req.body.user.email;
@@ -18,8 +21,16 @@ router.post('/users',function(req,res,next){
     }).catch(next);
 });
 
+router.get('/', auth.required, function(req, res, next){
+    User.findById(req.payload.id).then(function(user){
+        if(!user){return res.sendStatus(401); }
 
-router.post('/users/login', function(req, res, next){
+        return res.json({user:user.toProfileJSONFor()});
+    }).catch(next); 
+});
+
+
+router.post('/login', function(req, res, next){
     if(!req.body.user.username){
         return res.status(422).json({errors: {username:"can't be blank"}});
     }
@@ -39,12 +50,29 @@ router.post('/users/login', function(req, res, next){
         }
     })(req, res, next);
 });
- router.get('/user', auth.required, function(req, res, next){
+
+router.post('/addBook', auth.required, function(req,res,next){
+    console.log('calling add book.....');
     User.findById(req.payload.id).then(function(user){
         if(!user){return res.sendStatus(401); }
+        
+        if(req.body.book !== null)
+        {
+           // const tempBook = new Book(req.body.book);
 
-        return res.json({user:user.toAuthJSON()});
-    }).catch(next); 
+            return Book.findOne({title: {"$eq": req.body.book.title}}, function(err,book){
+                if(book !== null){
+                    console.log(book);
+                    return user.addBook(book._id)
+                            .then(function(){
+                                console.log('adding book done');
+                            return res.json({user:user.toProfileJSONFor()});
+                            });
+                }
+            })
+        }
+    })
 });
+
 
 export default router;
